2315a2073a17d7059751e4c38102c2d5f6cfbe1d
Michael Haberler <git@mah.priv.at>
2016-08-22 14:23:09 -0400

    fix fsck, zturn
    not sure how to do the update-initramfs -u in the chroot

--- target/chroot/machinekit-jessie-zturn.sh
+++ target/chroot/machinekit-jessie-zturn.sh
@@ -585,6 +585,12 @@ GOVERNOR="performance"
 EOFcpufrequtils
 }
 
+fix_fsck_error() {
+    sudo mkdir -p /etc/systemd/system-generators
+    sudo ln -s /dev/null /etc/systemd/system-generators/systemd-gpt-auto-generator
+    #update-initramfs -u
+}
+
 is_this_qemu q
 
 early_git_repos
@@ -609,5 +615,6 @@ remove_machinekit_pkgs # so the runtime deps are there
 symlink_dtbo
 add_uboot_to_fstab
 set_governor
+fix_fsck_error
 unsecure_root
 #
